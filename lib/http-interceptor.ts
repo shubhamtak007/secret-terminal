9346203d0681f5f2815c5eb4466ccd115d003098
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-08-11 12:33:28 +0530

    refactor: remove refresh token code from interceptor

--- lib/http-interceptor.ts
+++ lib/http-interceptor.ts
@@ -22,12 +22,7 @@ export const setupInterceptors = (client: AxiosInstance) => {
             };
 
             if (error.response?.data && error.response.data.message) {
-                if (['Access token missing', 'Invalid or expired token'].includes(error.response.data.message)) {
-                    originalRequest.retry = true;
-                    await client.post(secretTerminalEndpoints.auth.refreshToken);
-                    return client(originalRequest);
-
-                } else if (!['Unauthorized'].includes(error.response.data.message)) {
+                if (!['Unauthorized'].includes(error.response.data.message)) {
                     toast.error(error.response.data.message, { className: 'error-toast' });
 
                 } else {
