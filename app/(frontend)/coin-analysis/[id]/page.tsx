88a1cceddd6d94f77f91cc4ece09d82cd6643abe
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-08-11 14:17:35 +0530

    feat(ui): update coin analysis page title

--- app/(frontend)/coin-analysis/[id]/page.tsx
+++ app/(frontend)/coin-analysis/[id]/page.tsx
@@ -1,10 +1,19 @@
 import { Suspense } from 'react';
 import CoinAnalysisContainer from '@/components/features/coin-analysis/coin-analysis-container';
 import Loading from './loading';
+import { Metadata } from 'next';
 
 type Props = {
     params: Promise<{ id: string }>
     searchParams: Promise<Record<string, string | string[]>>
+}
+
+export async function generateMetadata({ params }: Props) {
+    const { id } = await params;
+
+    return {
+        title: id.charAt(0).toUpperCase() + id.slice(1)
+    }
 }
 
 async function CoinAnalysis({ params }: Props) {
