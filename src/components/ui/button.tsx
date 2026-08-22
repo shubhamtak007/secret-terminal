26d1b5dd2ffa331e9c06b839a606e511b9defae3
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-08-22 18:58:33 +0530

    fix: update button and select component styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,8 +18,8 @@ const buttonVariants = cva(
                 default: "bg-primary text-primary-foreground hover:bg-primary/90",
                 destructive: `bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20
                         dark:focus-visible:ring-destructive/40 dark:bg-destructive/60`,
-                outline: `border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground
-                        dark:bg-input/30 dark:border-input dark:hover:bg-input/50`,
+                outline: `border-1 border-[var(--border-color)] bg-[var(--main-bg-color)] text-[var(--text-color)]
+                        hover:brightness-[97%] dark:hover:brightness-[120%]`,
                 secondary: `bg-[var(--grey-color-1)] text-[var(--text-color)] hover:brightness-[97%]
                         dark:hover:brightness-[110%]`,
                 ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
