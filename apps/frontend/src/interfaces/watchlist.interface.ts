8d4b8578df1a75c1c471430d21012a01d7c6a64e
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-09-08 15:02:55 +0530

    fix(watchlist-interface): update marketData type

--- apps/frontend/src/interfaces/watchlist.interface.ts
+++ apps/frontend/src/interfaces/watchlist.interface.ts
@@ -1,19 +1,19 @@
-import { CoingeckoCrypto } from "./coin.interface";
+import { StCoin } from "@secret-terminal/types/coin-list.types";
 
 interface Watchlist {
-    id?: string,
-    name: string,
-    description?: string | null
+    id?: string;
+    name: string;
+    description?: string | null;
 }
 
 interface WatchlistCoin {
-    id: string,
-    coinId: string,
-    name: string,
-    symbol: string,
-    imageUrl: string,
-    watchlistId: string,
-    marketData: CoingeckoCrypto
+    id: string;
+    coinId: string;
+    name: string;
+    symbol: string;
+    imageUrl: string;
+    watchlistId: string;
+    marketData: StCoin;
 }
 
 export type { Watchlist, WatchlistCoin };
